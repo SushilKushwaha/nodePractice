69c19f2435a37087fe57482da15c9e3fea9bf7fa
SushilKushwaha <sushilsinha733@gmail.com>
2023-12-31 20:28:19 +0545

    this is the code for node practice

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const users =[
+    {name:'ram', address:'ktm'},
+    {name:'kaylin', address:'gorkha'},
+    {name:'bishwo', address:'gorkha'},
+    {name:'gopal', address:'ktm'}
+  ]
+
+app.get('/users:id', (req, res) => {
+   const filteredUser =  users.filter((item,id)=>{
+      if(item.name[0] ===req.query.startswith &&  item.name[item.name.length-1] ===req.query.endswith){
+        return item
+      }  
+    })
+    res.send(filteredUser)
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
